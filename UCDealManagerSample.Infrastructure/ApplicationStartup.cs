52e149c3708dce19574239e3c996ca1342721c96
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-10-12 17:03:30 +0200

    added modules registation methods

---
+++ UCDealManagerSample.Infrastructure/ApplicationStartup.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac.Extras.CommonServiceLocator;
+using CommonServiceLocator;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace UCDealManagerSample.Infrastructure
+{
+    public class ApplicationStartup
+    {
+        public static IServiceProvider Inizialize(IServiceCollection services, string connectionString)
+        {
+            return RegisterAutofacServiceProvider(services, connectionString);
+        }
+
+        private static IServiceProvider RegisterAutofacServiceProvider(IServiceCollection services, string connectionString)
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.Populate(services);
+
+            // add module registration
+
+            
+            //
+            var containeBuilded = containerBuilder.Build();
+
+            ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(containeBuilded));
+
+            var autofactServiceProvider = new AutofacServiceProvider(containeBuilded);
+
+            return autofactServiceProvider;
+
+        }
+    }
+}
---
+++ UCDealManagerSample.Infrastructure/CompositionRoot.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UCDealManagerSample.Infrastructure
+{
+    public static class CompositionRoot
+    {
+
+        private static Autofac.IContainer _container;
+
+        public static void SetContainer(Autofac.IContainer container)
+        {
+            _container = container;
+        }
+
+        internal static ILifetimeScope BeginLifeTimeScope()
+        {
+            return _container.BeginLifetimeScope();
+        }
+    }
+}
